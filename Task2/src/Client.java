1b25587bd5b3434440becb812468f8378f907b45
Michail19 <Mi.ershov.2017@yandex.ru>
2025-10-10 11:00:45 +0300

    Recompile

--- Task2/src/Client.java
+++ Task2/src/Client.java
@@ -6,6 +6,8 @@ public class Client {
 
     public static void main(String[] args) {
         try {
+            System.out.println();
+            
             Registry registry = LocateRegistry.getRegistry(null);
             Expression stub = (Expression) registry.lookup("Expression");
             double a = 1, b = -3, c = 2;
@@ -13,6 +15,23 @@ public class Client {
             Result result = stub.solve(a, b, c);
             System.out.println("Solve: " + result);
             System.out.println("Comment: " + result.getMessage());
+            System.out.println();
+
+            a = 1;
+            b = 2;
+            c = 1;
+            result = stub.solve(a, b, c);
+            System.out.println("Solve: " + result);
+            System.out.println("Comment: " + result.getMessage());
+            System.out.println();
+
+            a = 3;
+            b = 2;
+            c = 1;
+            result = stub.solve(a, b, c);
+            System.out.println("Solve: " + result);
+            System.out.println("Comment: " + result.getMessage());
+            System.out.println();
         }
         catch (Exception e) {
             System.err.println("Error: " + e);
