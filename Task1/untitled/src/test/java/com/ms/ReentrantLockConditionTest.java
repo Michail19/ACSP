10322ae4a7bc2c4d1e882c7b07a1478fd600b8d9
Michail19 <Mi.ershov.2017@yandex.ru>
2025-11-09 16:10:33 +0300

    U

---
+++ Task1/untitled/src/test/java/com/ms/ReentrantLockConditionTest.java
@@ -0,0 +1,20 @@
+package com.ms;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ReentrantLockConditionTest {
+
+    @Test
+    public void testPingPongExecution() throws InterruptedException {
+        // Тест для проверки корректности выполнения
+        Thread testThread = new Thread(() -> {
+            ReentrantLockCondition.main(new String[]{});
+        });
+
+        testThread.start();
+        testThread.join(5000); // Ждем максимум 5 секунд
+
+        assertFalse(testThread.isAlive(), "Программа должна завершиться");
+    }
+}
