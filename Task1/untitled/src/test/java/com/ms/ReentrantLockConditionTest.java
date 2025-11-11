d4b679779485d7a9a4eb3551f9b1c474e9472a37
Michail19 <Mi.ershov.2017@yandex.ru>
2025-11-11 19:50:38 +0300

    Update ReentrantLockConditionTest.java

--- Task1/untitled/src/test/java/com/ms/ReentrantLockConditionTest.java
+++ Task1/untitled/src/test/java/com/ms/ReentrantLockConditionTest.java
@@ -1,7 +1,7 @@
 package com.ms;
 
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 public class ReentrantLockConditionTest {
 
